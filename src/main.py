f9c29cfda6ecd3df6425447ac5a0299216f89c9e
Jerry Lee <oojerryleeoo@gmail.com>
2023-10-27 02:13:57 +0800

    Add UI

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from ui.mainwindow import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    form = MainWindow()
+    form.show()
+    app.exec_()
